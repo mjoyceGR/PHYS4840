782b0bf572be2a46df5a22e18a44ebe61e6b2807
Meridith Joyce <meridith.joyce@gmail.com>
2025-02-11 13:17:41 -0700

    template for time test in-class exercise

---
+++ Class07/time_test_template.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3.8
+####################################################
+#
+# Author: M Joyce
+#
+####################################################
+import time
+import numpy as np
+import sys
+import pandas as pd
+
+
+###################################################
+#
+# testing np.loadtxt()
+#
+###################################################
+start_numpy = time.perf_counter()
+
+"""
+put the action you want to time between the
+star and end commands
+"""
+
+start = time.perf_counter()
+blue, green, red, probability = np.loadtxt(filename, usecols=(8, 14, 26, 32), unpack=True)
+print("len(green): ", len(green))
+
+end_numpy  = time.perf_counter()
+
+print('Time to run loadtxt version: ', end_numpy-start_numpy, ' seconds')
+
+
+
+###################################################
+#
+# testing custom parser
+#
+###################################################
+start_parser = ...
+
+"""
+insert the custom parsing code
+"""
+
+end_parser  = ...
+
+print('Time to run custom parser version: ', end_parser-start_parser, ' seconds')
+
+
+###################################################
+#
+# testing pandas
+#
+###################################################
+start_pandas = ...
+
+"""
+insert the pandas parsing code
+"""
+
+end_pandas  = ...
+
+print('Time to run pandas version: ', end_pandas-start_pandas, ' seconds')
