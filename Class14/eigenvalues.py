49216cdbec43a5c443c4d1711900e7a591cbecca
Meridith Joyce <meridith.joyce@gmail.com>
2025-03-06 12:33:38 -0700

    exercises for Class 14

---
+++ Class14/eigenvalues.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3.8
+#####################################
+#
+# Class 13: Matrices and Linear algebra 
+# Author: M Joyce
+#
+#####################################
+import numpy as np
+from numpy import array,empty
+
+A = np.array([ [2, -1, 3,],\
+			   [-1, 4, 5], 
+			   [3,  5, 6] ],float)
+
+eigenvector_1 =  np.array([-0.5774,\
+						   -0.5774,\
+						   0.5774],float)
+
+LHS = np.dot(A, eigenvector_1)
+
+## Bonus: Why doesn't this line work??
+#LHS = A*eigenvector_1
+
+RHS = -2.0*eigenvector_1
+
+print("LHS:\n",LHS, "\n\nRHS:\n",RHS)
