3b8a171eb5152c287f0a81592ba85b751a38f473
Meridith Joyce <meridith.joyce@gmail.com>
2025-04-03 13:59:45 -0600

    run procedure in bash

--- Class20/run.sh
+++ Class20/run.sh
@@ -1,6 +1,12 @@
 !#/usr/bin/bash
 
 python3.8 double_pendulum.py
-python3.8 plot_double_pendulum_data.py
+#python3.8 perturbed_pendulum.py
+#python3.8 plot_double_pendulum_data.py
+#eog motions.png
 
-eog motions.png
+## this will generate a video of the simulation with the name specified in generate_video.py
+## it will generate this video based on the data file provided in generate_video.py
+python3.8 generate_video.py 
+
+vlc double_pendulum_simulation.mp4
