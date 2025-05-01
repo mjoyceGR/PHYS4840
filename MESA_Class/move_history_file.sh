8af7f0cd14dc2f37cd56097f869aa2be1f4a792c
Meridith Joyce <meridith.joyce@gmail.com>
2025-05-01 12:07:10 -0600

    MESA class cp script

---
+++ MESA_Class/move_history_file.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## YOU MUST PUT YOUR OWN USER NAME IN THIS COMMAND
+## do not use "mjoyce8"
+
+scp -r mjoyce8@medicinebow.arcc.uwyo.edu:/home/mjoyce8/1M_pre_ms_to_wd/LOGS/history.data .
